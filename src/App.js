b101eeabcc6942ccdef6a161bb2a5fb539a12140
Cian Hayes <cian.m.hayes@gmail.com>
2020-09-07 12:41:25 -0700

    Some simple visualizations

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useImperativeHandle } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Sketch from "react-p5";
+import { setTensorTracker } from '@tensorflow/tfjs-core/dist/tensor';
+import * as posenet from '@tensorflow-models/posenet';
+
+import PoseSource from './components/pose_source';
+
 import './App.css';
+
+class ProcesingTest extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.p5Setup = this.p5Setup.bind(this);
+    this.p5Draw = this.p5Draw.bind(this);
+    this.notifyNewPose = this.notifyNewPose.bind(this);
+    this.currentPose = { keypoints: [] };
+    this.processingScale = this.props.processingScale ? this.props.processingScale : 1;
+  }
+
+  notifyNewPose(pose) {
+    this.currentPose = pose;
+  }
+
+  p5Setup(p5, canvasParentRef) {
+    p5.createCanvas(700 * this.processingScale, 700 * this.processingScale).parent(canvasParentRef);
+  }
+
+  p5Draw(p5) {
+    p5.background(0);
+
+    p5.stroke(p5.color('aqua'));
+    p5.strokeWeight(3);
+    if (this.currentPose.keypoints.length > 0) {
+      const adjacentKeyPoints = posenet.getAdjacentKeyPoints(this.currentPose.keypoints, 0.4);
+      for (var i = 0; i < adjacentKeyPoints.length; i++) {
+        p5.line(
+          adjacentKeyPoints[i][0].position.x * this.processingScale,
+          adjacentKeyPoints[i][0].position.y * this.processingScale,
+          adjacentKeyPoints[i][1].position.x * this.processingScale,
+          adjacentKeyPoints[i][1].position.y * this.processingScale);
+      }
+    }
+
+    p5.strokeWeight(7);
+    for (var i = 0; i < this.currentPose.keypoints.length; i++) {
+      if (this.currentPose.keypoints[i].score > 0.4) {
+        p5.point(
+          this.currentPose.keypoints[i].position.x * this.processingScale,
+          this.currentPose.keypoints[i].position.y * this.processingScale);
+      }
+    }
+
+  }
+
+  render() {
+    return (
+      <div>
+        <PoseSource
+          showVideo={this.props.showVideo ? this.props.showVideo : false}
+          markupVideo={this.props.showVideo ? this.props.showVideo : false}
+          flipHorizontal
+          videoWidth={700}
+          onPoseUpdated={this.notifyNewPose} />
+        <Sketch setup={this.p5Setup} draw={this.p5Draw} />
+      </div>);
+  }
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/basic-with-video">
+            <ProcesingTest showVideo/>
+          </Route>
+          <Route path="/basic-without-video">
+          <ProcesingTest processingScale={2}/>
+          </Route>
+          <Route path="/">
+            <p>
+            <Link to="/basic-with-video">Basic visualization and source video together.</Link>
+            </p>
+            <p>
+            <Link to="/basic-without-video">Basic visualization alone.</Link>
+            </p>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/pose_source.js
@@ -0,0 +1,125 @@
+import React, { useImperativeHandle } from 'react';
+import * as posenet from '@tensorflow-models/posenet';
+import * as tf from '@tensorflow/tfjs';
+
+class PoseSource extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        running: false,
+        flipHorizontal: true,
+        showVideo: true,
+        markupVideo: true
+      };
+  
+      this.startCamera = this.startCamera.bind(this);
+      this.detectPoses = this.detectPoses.bind(this);
+  
+      this.videoWidth = this.props.videoWidth;
+      this.videoHeight = this.props.videoHeight ? this.props.videoHeight : this.videoWidth;
+    }
+  
+    componentDidMount(){
+      this.startCamera();
+      
+    }
+
+    componentWillUnmount(){
+        this.mediaStream.getTracks().forEach(function(track) {
+            track.stop();
+          });
+    }
+  
+    async startCamera() {
+  
+      this.poseEstimator = await posenet.load(
+        {
+          architecture: "MobileNetV1",
+          outputStride: 16,
+          inputResolution:
+          {
+            width: this.videoWidth,
+            height: this.videoHeight
+          },
+          multiplier: 0.75
+        });
+      var constraints = { audio: false, video: { width: this.videoWidth, height: this.videoHeight } };
+      this.mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
+      this.videoElement = document.getElementById("videoElement");
+      this.videoElement.width = this.videoWidth;
+      this.videoElement.height = this.videoHeight;
+      this.videoElement.srcObject = this.mediaStream;
+      this.videoElement.onloadeddata = this.detectPoses;
+  
+      if (this.props.showVideo) {
+        this.outputCanvas = document.getElementById("videoOutput");
+        this.outputCanvas.width = this.videoWidth;
+        this.outputCanvas.height = this.videoHeight;
+        this.outputCanvasContext = this.outputCanvas.getContext("2d");
+      }
+  
+      this.setState({ running: true });
+    }
+  
+    async detectPoses() {
+      const pose = await this.poseEstimator.estimateSinglePose(this.videoElement, {
+        flipHorizontal: this.state.flipHorizontal,
+        decodingMethod: 'single-person'
+      });
+      if (this.props.onPoseUpdated) {
+        this.props.onPoseUpdated(pose);
+      }
+      if (this.props.showVideo) {
+        this.outputCanvasContext.clearRect(0, 0, this.videoWidth, this.videoHeight);
+        this.outputCanvasContext.save();
+        if (this.props.flipHorizontal) {
+          this.outputCanvasContext.scale(-1, 1);
+          this.outputCanvasContext.translate(-this.videoWidth, 0);
+        }
+        this.outputCanvasContext.drawImage(this.videoElement, 0, 0, this.videoWidth, this.videoHeight);
+        this.outputCanvasContext.restore();
+        if (this.props.markupVideo) {
+          for (var i = 0; i < pose.keypoints.length; i++) {
+            if (pose.keypoints[i].score > 0.4) {
+              this.outputCanvasContext.beginPath();
+              this.outputCanvasContext.arc(
+                pose.keypoints[i].position.x,
+                pose.keypoints[i].position.y,
+                3,
+                0,
+                2 * Math.PI);
+              this.outputCanvasContext.fillStyle = "aqua";
+              this.outputCanvasContext.fill();
+            }
+          }
+          const adjacentKeyPoints = posenet.getAdjacentKeyPoints(pose.keypoints, 0.4);
+          for(var i = 0; i < adjacentKeyPoints.length; i++){
+            this.outputCanvasContext.beginPath();
+            this.outputCanvasContext.moveTo(adjacentKeyPoints[i][0].position.x, adjacentKeyPoints[i][0].position.y);
+            this.outputCanvasContext.lineTo(adjacentKeyPoints[i][1].position.x, adjacentKeyPoints[i][1].position.y);
+            this.outputCanvasContext.lineWidth = 2;
+            this.outputCanvasContext.strokeStyle = "aqua";
+            this.outputCanvasContext.stroke();
+          }
+        }
+      }
+
+      if (this.state.running)
+      {
+        requestAnimationFrame(this.detectPoses);
+      }
+    }
+  
+    render() {
+      return (
+        <div>
+          <div>
+            <video id="videoElement" autoPlay={true} playsInline style={{ display: "none" }} />
+            <canvas id="videoOutput" />
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  export default PoseSource;
